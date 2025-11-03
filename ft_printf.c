31204e84bb8a2a07f6778e4a9d908f7bae746ebc
Mohamed Jaber <mjaber@c1r2p12.1337.ma>
2025-11-03 14:17:11 +0100

    fix norms

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,32 @@
 /*   By: mjaber <mjaber@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 18:55:49 by mjaber            #+#    #+#             */
-/*   Updated: 2025/11/03 14:05:34 by mjaber           ###   ########.fr       */
+/*   Updated: 2025/11/03 14:15:22 by mjaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	ft_print_format(char c, va_list args)
+{
+	if (c == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	else if (c == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	else if (c == 'p')
+		return (ft_print_memory(va_arg(args, void *)));
+	else if (c == 'i' || c == 'd')
+		return (ft_putnbr_base(va_arg(args, int), "0123456789"));
+	else if (c == 'u')
+		return (ft_putunbr(va_arg(args, unsigned int)));
+	else if (c == 'x')
+		return (ft_putnbr_base(va_arg(args, unsigned int), "0123456789abcdef"));
+	else if (c == 'X')
+		return (ft_putnbr_base(va_arg(args, unsigned int), "0123456789ABCDEF"));
+	else if (c == '%')
+		return (ft_putchar('%'));
+	return (0);
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -25,30 +46,11 @@ int	ft_printf(const char *format, ...)
 	{
 		if (*format == '%' && *(format + 1))
 		{
-			if (*(format + 1) == 'c')
-				count += ft_putchar(va_arg(args, int));
-			else if (*(format + 1) == 's')
-				count += ft_putstr(va_arg(args, char *));
-			else if (*(format + 1) == 'p')
-				count += ft_print_memory(va_arg(args, void *));
-			else if (*(format + 1) == 'i' || *(format + 1) == 'd')
-				count += ft_putnbr_base(va_arg(args, int), "0123456789");
-			else if (*(format + 1) == 'u')
-				count += ft_putunbr(va_arg(args, unsigned int));
-			else if (*(format + 1) == 'x')
-				count += ft_putnbr_base(va_arg(args, unsigned int),
-						"0123456789abcdef");
-			else if (*(format + 1) == 'X')
-				count += ft_putnbr_base(va_arg(args, unsigned int),
-						"0123456789ABCDEF");
-			else if (*(format + 1) == '%')
-				count += ft_putchar('%');
+			count += ft_print_format(*(format + 1), args);
 			format++;
 		}
 		else if (*format != '%')
 			count += ft_putchar(*format);
-		if (*format == '\0')
-			break ;
 		format++;
 	}
 	va_end(args);
